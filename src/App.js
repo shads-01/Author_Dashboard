fe39fc64f0540c06b4ebd194313344600821e09d
Shahadat Hasan <tamimshahadat15@gmail.com>
2025-08-19 17:58:36 +0600

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
+import MyArticles from './pages/MyArticles';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <Sidebar/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Sidebar />}>
+          <Route index element={<MyArticles />} />
+          <Route path='profile' element={<Profile />} />
+          <Route path='articles' element={<MyArticles />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
